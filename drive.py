c8ef73cbc9199eebb227cc311663170d6d185c8c
Surya Sumukh <surya.sumukh@gmail.com>
2017-12-03 22:14:26 -0500

    feat: updated drive.py. modified code to convert images to grayscale and crop

--- drive.py
+++ drive.py
@@ -16,6 +16,8 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+import cv2
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
@@ -61,6 +63,9 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+        image_array = np.expand_dims(image_array, axis=2)
+        image_array = image_array[60: 140]
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
