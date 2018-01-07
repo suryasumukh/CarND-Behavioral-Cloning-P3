1136caa5e600e718c86da5e75f8dd5f36dbf5542
Surya Sumukh <surya.sumukh@gmail.com>
2018-01-07 11:53:22 -0500

    feat: updated utils to sample better from skewed distribution

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -23,36 +23,57 @@ def read_samples(data_dir):
 
 class ImageGenerator(Sequence):
     def __init__(self, samples, batch_size, corr=0.0):
-        self.samples = samples
         self.batch_size = batch_size
         self.corr = corr
 
-        self._len = len(samples)
+        self.samples = []
+        self.steering_angles = []
+
+        for sample in samples:
+            for img_path in sample[: 3]:
+                self.samples.append((False, img_path))
+                self.samples.append((True, img_path))
+            angle = float(sample[3])
+            angles = [angle, -angle, angle+corr, -(angle+corr), angle-corr, -(angle-corr)]
+            self.steering_angles.extend(angles)
+
+        self.steering_angles = np.array(self.steering_angles)
+        self._len = len(self.steering_angles)
+
+        self.samples, self.steering_angles = shuffle(self.samples, self.steering_angles)
 
     def __len__(self):
         return (self._len + self.batch_size - 1) // self.batch_size
 
+    def preprocess(self, sample):
+        flip, img_path = sample
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)[70: 140, :, 2]
+        if flip:
+            image = cv2.flip(image, 1)
+        image = cv2.resize(image, (320, 160))
+        return np.expand_dims(image, axis=2)
+
     def __getitem__(self, item):
-        x_batch = np.empty([self.batch_size * 3, 160, 320, 1])
-        y_batch = np.empty([self.batch_size * 3])
+        x_batch = np.empty([self.batch_size, 160, 320, 1])
+        y_batch = []
         i = 0
-        for sample in self.samples[item: item + self.batch_size]:
-            angle = float(sample[3])
-            angles = [angle, angle+self.corr, angle-self.corr]
-            for img_path, angle, flip in zip(sample[:3], angles, np.random.choice(2, 3)):
-                img = cv2.imread(img_path)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)[:, :, 2]
-                if flip:
-                    img = cv2.flip(img, 1)
-                    angle = angle * -1.0
-                x_batch[i] = np.expand_dims(img, axis=2)
-                y_batch[i] = angle
+        while len(y_batch) < self.batch_size:
+            idx = np.random.choice(self._len)
+            if np.absolute(self.steering_angles[idx]) <= 0.2:
+                if np.random.uniform() < 0.3:
+                    y_batch.append(self.steering_angles[idx])
+                    x_batch[i] = self.preprocess(self.samples[idx])
+                    i += 1
+            else:
+                y_batch.append(self.steering_angles[idx])
+                x_batch[i] = self.preprocess(self.samples[idx])
                 i += 1
 
-        return x_batch, y_batch
+        return x_batch, np.array(y_batch)
 
     def on_epoch_end(self):
-        self.samples = shuffle(self.samples)
+        self.samples, self.steering_angles = shuffle(self.samples, self.steering_angles)
 
 
 class CenterImageGenerator(Sequence):
